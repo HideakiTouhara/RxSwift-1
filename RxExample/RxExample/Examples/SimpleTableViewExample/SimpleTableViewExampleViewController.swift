7c4f3f5b28a97e0e96d7592ed4bd30e179cfd490
HideakiTouhara <hideee6723@gmail.com>
2017-10-17 15:08:35 +0900

    comment out

--- RxExample/RxExample/Examples/SimpleTableViewExample/SimpleTableViewExampleViewController.swift
+++ RxExample/RxExample/Examples/SimpleTableViewExample/SimpleTableViewExampleViewController.swift
@@ -18,30 +18,32 @@ class SimpleTableViewExampleViewController : ViewController, UITableViewDelegate
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let items = Observable.just(
-            (0..<20).map { "\($0)" }
-        )
-
-        items
-            .bind(to: tableView.rx.items(cellIdentifier: "Cell", cellType: UITableViewCell.self)) { (row, element, cell) in
-                cell.textLabel?.text = "\(element) @ row \(row)"
-            }
-            .disposed(by: disposeBag)
-
-
-        tableView.rx
-            .modelSelected(String.self)
-            .subscribe(onNext:  { value in
-                DefaultWireframe.presentAlert("Tapped `\(value)`")
-            })
-            .disposed(by: disposeBag)
-
-        tableView.rx
-            .itemAccessoryButtonTapped
-            .subscribe(onNext: { indexPath in
-                DefaultWireframe.presentAlert("Tapped Detail @ \(indexPath.section),\(indexPath.row)")
-            })
-            .disposed(by: disposeBag)
+//        let items = Observable.just(
+//            (0..<20).map { "\($0)" }
+//        )
+//
+//        items
+//            .bind(to: tableView.rx.items(cellIdentifier: "Cell", cellType: UITableViewCell.self)) { (row, element, cell) in
+//                cell.textLabel?.text = "\(element) @ row \(row)"
+//            }
+//            .disposed(by: disposeBag)
+//
+//
+//        tableView.rx
+//            .modelSelected(String.self)
+//            .subscribe(onNext:  { value in
+//                DefaultWireframe.presentAlert("Tapped `\(value)`")
+//            })
+//            .disposed(by: disposeBag)
+//
+//        tableView.rx
+//            .itemAccessoryButtonTapped
+//            .subscribe(onNext: { indexPath in
+//                DefaultWireframe.presentAlert("Tapped Detail @ \(indexPath.section),\(indexPath.row)")
+//            })
+//            .disposed(by: disposeBag)
+        
+        
 
     }
 
