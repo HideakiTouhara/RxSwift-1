ac9e8de3f27e7c0bf27b33703bec9498a2e6bbc8
HideakiTouhara <hideee6723@gmail.com>
2017-10-17 15:55:51 +0900

    rewrite simpletableviewexample

--- RxExample/RxExample/Examples/SimpleTableViewExample/SimpleTableViewExampleViewController.swift
+++ RxExample/RxExample/Examples/SimpleTableViewExample/SimpleTableViewExampleViewController.swift
@@ -43,7 +43,28 @@ class SimpleTableViewExampleViewController : ViewController, UITableViewDelegate
 //            })
 //            .disposed(by: disposeBag)
         
+        let items = Observable.just(
+            (0..<20).map { "\($0)" }
+        )
         
+        items.bind(to: tableView.rx.items(cellIdentifier: "Cell", cellType: UITableViewCell.self)) { (row, element, cell) in
+            cell.textLabel?.text = "\(element) @ row \(row)"
+        }
+        .disposed(by: disposeBag)
+        
+        tableView.rx
+            .modelSelected(String.self)
+            .subscribe(onNext: { value in
+                DefaultWireframe.presentAlert("Tapped \(value)")
+            })
+            .disposed(by: disposeBag)
+        
+        tableView.rx
+            .itemAccessoryButtonTapped
+            .subscribe(onNext: { indexPath in
+                DefaultWireframe.presentAlert("Tapped Detail @ \(indexPath.section),\(indexPath.row)")
+            })
+            .disposed(by: disposeBag)
 
     }
 
